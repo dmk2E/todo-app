8e083be503b326c0c2235c9d7eb6f0db4d50a47b
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-15 13:11:52 +0000

    Finish user signup #2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = 'Welcome to the Todo App!'
+      redirect_to @user
+      # redirect_to user_url(@user)
     else
       render 'new', status: :unprocessable_entity
     end

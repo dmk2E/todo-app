c646db0dfecb001ee217af767abaa9a6e8da7bba
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-15 07:38:50 +0000

    Add some assertion test #1

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,11 +3,15 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @base_title = 'Todo App'
+    @account_link = 'https://github.com/dmk2E'
   end
   test 'should get to root' do
     get root_path
     assert_response :success
     assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', '#'
+    assert_select 'a[href=?]', '#'
+    assert_select 'a[href=?]', @account_link
     assert_select 'title', full_title()
   end
 end

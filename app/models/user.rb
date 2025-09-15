2393a0dcdfc376da9cef1138bac8fc398dc92e64
KouE10e <210100037+dmk2E@users.noreply.github.com>
2025-09-15 14:06:02 +0000

    Create Todo model #7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :todos, dependent: :destroy
+
   validates :name, presence: true, uniqueness: true
   has_secure_password
   validates :password, length:{minimum: 6}, presence: true, allow_nil: true
